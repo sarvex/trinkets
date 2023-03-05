5867f2bbadc872277dda287c15bd9690988ca319
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-03-04 19:36:46 -0800

    Frick it let's make this in C

--- scripts/buildgcc.sh
+++ scripts/buildgcc.sh
@@ -1,14 +1,33 @@
-#buildgcc.sh: a user-friendly installation script for GCC
-#Designed for GCC 12.2
+#!/bin/bash
 
-while getopts a: flag
-do
-  case "${flag}" in
-    a) arg=${OPTARG};;
-  esac
-done
+# buildgcc.sh: a user-friendly installation script for GCC
+# Designed for GCC 12.2
 
-echo $arg
+# Get flags (I actually have no idea what this does but it works)
+# while getopts a: flag
+# do
+#   case "${flag}" in
+#     a) arg=${OPTARG};;
+#   esac
+# done
 
-printf "\n\n\033[0mThank you for using HackerDaGreat57's GCC Build Script. Exiting...\n"
+# echo $1
+
+check_args() {
+  # case $1 in
+  #   '-h' | '--help')
+  #     printf "Welcome to the Help manual."
+  #     ;;
+  #   *)
+  #     printf "Unrecognized argument. Check spelling or rerun with --help."
+  #     ;;
+  # esac
+  if [ "$1" = "-h" ]; then
+    printf "tea"
+  fi
+}
+
+check_args
+
+printf "\033[0m\n"
 exit 0
