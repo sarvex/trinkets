a95ee0ba705615bf1f681d53ccc3d1fce1a8fbba
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-03-10 12:38:17 -0800

    Not using separate function for argument checking

--- trinkets/buildgcc/src/bgcc.c
+++ trinkets/buildgcc/src/bgcc.c
@@ -9,7 +9,7 @@ void check_args(short int *wtd); /* wtd = What To Do code */
 short int m_wtd; /* m is there to differentiate between check_args()'s        */
                  /* pointer and the actual variable ('m' is chosen randomly)  */
 
-union wtd {       /* This union contains variables that are to be used in     */
+union s_wtd {     /* This union contains variables that are to be used in     */
                   /* cases that help the program figure out what to do. Each  */
                   /* What-To-Do Code indicates a specific thing. A reference  */
                   /* list is in Annex A, at the bottom of the source code     */
@@ -19,29 +19,23 @@ union wtd {       /* This union contains variables that are to be used in     */
   short int p_args; /* Program arguments */
   short int f_ret;  /* Function return code (generally used for anything that */
                     /* isn't related to program arguments)                    */
-};
+} wtd_c; /* We will acess this as wtc_c */
 
 /* main: get the program started and figure out what to do */
 int main(int argc, char *argv[]) {
   check_args(&m_wtd); /* Check arguments */
-  
-  printf("Arguments passed: %d, %s\n", argc, argv[0]);
-
-  int i;
-  for (i=0; i< argc; i++) {
-     printf("\narg%d=%s", i, argv[i]);
-  }
 
   return 0;
 }
 
 /* check_args: check arguments passed to the program */
 void check_args(short int *wtd) {
-  /* *wtd = 5; */
+  wtd_c.p_args = 5;
+  printf("%d", wtd_c.p_args);
 }
 
 
 /* Annex A: What-To-Do Code Reference
 1. Print help information about the program
-
+2. Print version information
 */
