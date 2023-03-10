c9202adf722b3ba8001d778c31ff3124a814fd44
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-03-09 21:37:08 -0800

    Sweet sweet progress...

--- trinkets/buildgcc/src/bgcc.c
+++ trinkets/buildgcc/src/bgcc.c
@@ -3,18 +3,38 @@
 #include <curses.h> /* For user-friendly mode */
 
 /* Function declarations for functions below main */
-void check_args(short int *wtd); /* WTD = What To Do code */
+void check_args(short int *wtd); /* wtd = What To Do code */
+
+/* Data and variable stuff */
+short int m_wtd; /* m is there to differentiate between check_args()'s        */
+                 /* pointer and the actual variable ('m' is chosen randomly)  */
+
+union wtd {       /* This union contains variables that are to be used in     */
+  short int args; /* cases that tell the program figure out what to do. Each  */
+  short int ret;  /* What-To-Do Code indicates a specific thing. A list is    */
+};                /* in Annex A, at the bottom of the source code file.       */
 
 /* main: get the program started and figure out what to do */
 int main(int argc, char *argv[]) {
-  short int m_wtd;
   check_args(&m_wtd); /* Check arguments */
-  printf("Arguments: %d", m_wtd);
+  
+  printf("Arguments passed: %d, %s\n", argc, argv[0]);
+
+  int i;
+  for (i=0; i< argc; i++) {
+     printf("\narg%d=%s", i, argv[i]);
+  }
 
   return 0;
 }
 
 /* check_args: check arguments passed to the program */
 void check_args(short int *wtd) {
-  *wtd = 5;
+  /* *wtd = 5; */
 }
+
+
+/* Annex A: What-To-Do Code Reference
+1. Print help information about the program
+
+*/
