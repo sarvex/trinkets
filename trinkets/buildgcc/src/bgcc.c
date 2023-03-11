c63826dd6317d4d2b1af5ec4519dd02332b22be3
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-03-10 16:12:32 -0800

    Set up debugging environment

--- trinkets/buildgcc/src/bgcc.c
+++ trinkets/buildgcc/src/bgcc.c
@@ -1,12 +1,12 @@
 /* Includes */
 #include <stdio.h> /* Standard input/output library */
-#include <curses.h> /* For user-friendly mode */
+/* #include <curses.h> /* For user-friendly mode */
 
 /* Function declarations for functions below main */
-/*void check_args(short int *wtd, int *ac, char *av); /* wtd = What To Do code */
+void check_args(short int *wtd); /* wtd = What To Do code */
 
 /* Data and variable stuff */
-union s_wtd {     /* This union contains variables that are to be used in     */
+union u_wtd {     /* This union contains variables that are to be used in     */
                   /* cases that help the program figure out what to do. Each  */
                   /* What-To-Do Code indicates a specific thing. A reference  */
                   /* list is in Annex A, at the bottom of the source code     */
@@ -18,28 +18,38 @@ union s_wtd {     /* This union contains variables that are to be used in     */
                     /* isn't related to program arguments)                    */
 } wtd_c; /* We will acess this as wtc_c */
 
+struct s_args {
+  int argc;
+  char argv[];
+} args;
+
 /* main: get the program started and figure out what to do */
 int main(int argc, char *argv[]) {
-  /*check_args(wtd_c.p_args, &argc, &argv);*/
-  int i;
-  for (i = 0; i < argc; i++) {
-    printf("\narg%d=%s", i, argv[i]);
+  args.argc = argc;
+  int i; for (i = 0; i < argc; i++) {
+    args.argv[i] = *argv[i];
   }
 
-  printf("\n\n%d %s\n\n", argc, argv[1]);
+  check_args(wtd_c.p_args);
+  /* for (i = 0; i < argc; i++) {
+    printf("\narg%d=%s", i, argv[i]);
+  } */
+
+  /* printf("\n\n%d %s\n\n", argc, argv[1]); */
 
   /* This will run if arguments are not recognized */
-  printf("\nUnknown argument(s) '-h'. Please fix it and try again.");
+  /* printf("\nUnknown argument(s) '-h'. Please fix it and try again."); */
   return 0;
 }
 
 /* check_args: check arguments passed to the program */
-/*void check_args(short int *wtd, int *ac, char *av) {
+void check_args(short int *wtd) {
   int i;
-  for (i = 0; i < ac; i++) {
-    printf("\narg%d=%s", i, av[i]);
+  printf("ee\n\n");
+  for (i = 0; i < args.argc; i++) {
+    printf("\narg%d=%s", i, args.argv[i]);
   }
-}*/
+}
 
 
 /* Annex A: What-To-Do Code Reference
