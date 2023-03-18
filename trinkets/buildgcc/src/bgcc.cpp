5d1102d23c3a7fd4d6e25d510e6457ec82f3127e
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-03-17 20:51:17 -0700

    Lots of progress on planning; switched to another format

--- trinkets/buildgcc/src/bgcc.cpp
+++ trinkets/buildgcc/src/bgcc.cpp
@@ -18,19 +18,45 @@ int main(int argc, char *argv[]) { //main: get the program started and figure ou
   return 0; //This will never actually run, it's just there for standards compliance 
 }
 
+/*pair<string, string> reg_foo(const string& s) //Custom argument parsing (from https://github.com/boostorg/program_options/blob/develop/example/custom_syntax.cpp)
+{
+    if (s.find("-") == 0) {
+        if (s.substr(2, 3) == "no-")
+            return make_pair(s.substr(5), string("false"));
+        else
+            return make_pair(s.substr(2), string("true"));
+    } else {
+        return make_pair(string(), string());
+    }
+}*/
+
+std::pair<std::string, std::string> custom_argparse(const std::string& s) //Custom argument parsing (modified version of https://github.com/boostorg/program_options/blob/develop/example/custom_syntax.cpp)
+{
+    if (s.find("-f") == 0) {
+        if (s.substr(2, 3) == "ec")
+            return std::make_pair(s.substr(5), std::string("false"));
+        else
+            return std::make_pair(s.substr(2), std::string("true"));
+    } else {
+        return std::make_pair(std::string(), std::string());
+    }    
+}
+
 void check_params(int pargc, char **pargv) { //check_params: check arguments/parameters/options passed to the program
   short int errorcode; //Taken as argument for --errorcode
 
   try {
     boost::program_options::options_description opt_general("General options", 100); //Create general options set
     opt_general.add_options() //Add options to the general set...
-      ("help", "Print help message (the thing that you're reading right now)")
+      ("help,h", "Print help message (the thing that you're reading right now)")
       ("errorhelp", "Print basic information about all error codes")
       ("errorcode", boost::program_options::value(&errorcode), "Displays a longer description for a specific error code with possible solutions. Replace 'arg' with your error code");
     
     boost::program_options::options_description opt_automation("Automation options", 100); //Create automation options set
     opt_automation.add_options() //Add options to the automation set...
-      ("outdir", "The directory where I should save your build");
+      ("outdir", "The directory where your build will be saved")
+      ("srcdir", "The directory where GCC source code is located. This program comes with GCC 12.2 sources included, but you can compile it from other sources if you'd like.")
+      ("objdir", "The directory where object files will be placed.");
 
     #ifdef P_DEV
     boost::program_options::options_description opt_visible("[DEVELOPMENT BUILD] All options for buildgcc vW.X.Y.Z", 100); /*****************************************/
@@ -41,13 +67,15 @@ void check_params(int pargc, char **pargv) { //check_params: check arguments/par
 
     boost::program_options::options_description opt_all("All allowed options", 100); //Every single option that exists
     opt_all.add(opt_general).add(opt_automation);
-
+    /*using namespace std;
+    using namespace boost::program_options;*/
     boost::program_options::variables_map p_variables_map;
     boost::program_options::store(boost::program_options::parse_command_line(pargc, pargv, opt_all), p_variables_map); //The program needs to know about ALL arguments but the user usually needs only the default visible ones
     boost::program_options::notify(p_variables_map); //Set it up and get ready for parsing
+    boost::program_options::store(boost::program_options::command_line_parser(pargc, pargv).options(opt_all).extra_parser(custom_argparse).run(), p_variables_map);
 
     if (p_variables_map.count("help")) { //--help
-      std::cout << opt_visible << std::endl;
+      std::cout << opt_visible << std::endl << std::endl << "Remember, you can run 'bgcc --auto' to get fully automated compilation into default directories like /bin, etc. Refer to the official GCC configuration options reference for more details." << std::endl << std::endl;
       exit(0);
     }
 
